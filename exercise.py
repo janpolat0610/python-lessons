0bda3fc4f102840829c2376fad7dcae3d29c4abb
janpolat0610 <janpolatelmuratov2010@gmail.com>
2024-07-29 17:46:17 +0500

    shinigiwlar

---
+++ exercise.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 29 16:27:44 2024
+
+@author: User
+"""
+
+# car_0={'cars':'ferrari','ren':'qizil'}
+# print(car_0['cars'])
+# print(car_0['ren'])
+
+
+# en_uz={'apple':'alma','apricot':'erik'}
+
+# miywe={'alma':10000,'garbiz':700,'juzim':20000}
+# print(f"Alma bahasi {miywe['alma']} som")
+
+
+# student={'ati familyasi':'Ernur','jasi':14,'t_jili':2010}
+# print(f"student {student['ati familyasi']},jasi {student['jasi']} de, {student['t_jili']} -jili tuwilgan")
+# student['kurs']=4
+# student['fakultet']='IT'
+# student['ati familyasi']='Bayram'
+
+
+

2f1c92f3336aee8139651902dadd4712fde32d09
janpolat0610 <janpolatelmuratov2010@gmail.com>
2024-08-03 12:26:13 +0500

    Update exercise.py

--- exercise.py
+++ exercise.py
@@ -4,7 +4,7 @@ Created on Mon Jul 29 16:27:44 2024
 
 @author: User
 """
-print('Hello World')
+# print('Hello World')
 
 
 
@@ -25,6 +25,87 @@ print('Hello World')
 # student['kurs']=4
 # student['fakultet']='IT'
 # student['ati familyasi']='Bayram'
+
+# print(student.items())
+# for kalit,qiymat in student.items():
+#     print(f"kalit:{kalit}")
+#     print(f'qiymat:{qiymat}\n')
+    
+# telefonlar={
+#     'ali':'iphone x',
+#     'vali':'galaxy s10',
+    # 'sarvar':'mi 10'}
+# for k,q in telefonlar.items():
+#     print(f'{k}ning telefoni {q}')
+# for k in telefonlar.keys():
+#     print(k)
+
+# for q in telefonlar.values():
+#     print(q)
+
+# ism=input('salom ismingiz nima?\n')
+# savol=f'salom {ism.title()}. yoshingiz nechida? \n'
+# yosh=input(savol)
+# yosh=int(yosh)
+# boy=input(f'{ism} boyingiz nechi metr? \n')
+# boy=float(boy)
+# print(f'ismi : {ism}',
+#       f'yoshi : {yosh}',
+#       f'boyi : {boy}')
+
+
+# son=1
+# while  son<=5:
+#     print(son, end=" ")
+#     son=son+1
+# print('Tawsildi')
+
+
+# print('sannin kvadratin shigariwshi programma')
+# san='san kiritin'
+# san += "(eger toqtatpaqshi bolsaniz 'exit' deb jazin )"
+# manis=' '
+# while manis != 'exit':
+#     manis=input(san)
+#     if manis != 'exit':
+#         print(float(manis)**2)
+# print('programma toqtadi')
+
+
+
+
+print('sannin kvadratin shigariwshi programma')
+san='san kiritin'
+san += "(eger toqtatpaqshi bolsaniz 'exit' deb jazin )"
+belgi=True
+while belgi:
+    manis=input(san)
+    if manis=='exit':
+        belgi=False
+    else:
+        print(float(manis)**2)
+print('programma toqtadi')
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

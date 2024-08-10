125e89c33ec72a9050d274a7f00a46919a14ed31
janpolat0610 <janpolatelmuratov2010@gmail.com>
2024-08-10 16:21:43 +0500

    Create untitled1.py

---
+++ untitled1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 10 15:36:12 2024
+
+@author: User
+"""
+
+from exercise import son_top
+from exercise import san_tap_pc
+from exercise import play
+
+print(play())
